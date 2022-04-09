40d8f8db0ee4211008c2eb9bb6435423dfecedf1
jassi <jassi@gmail.com>
2022-04-09 13:59:40 -0700

    monster fight

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ import random
 
 
 class Monster:
-    def __init__(self, hp = 10):
+    def __init__(self, hp = 30, hair_color, age, type):
         self.hp = hp
+        self.hair_color = hair_color
+        self.age = age
+        self.type = type
+
+    def get_hair_color(self):
+        return self.hair_color
+
+    def get_age(self):
+        return self.age
+
+    def get_type(self):
+        return self.type
 
     def attack(self, monster):
-        monster.get_attacked(3, self)
+        monster.get_attacked(1, 5, self)
 
     def get_attacked(self, damage, attacker):
+        damage = random.randint
         self.hp = self.hp - damage
         print("Monster got attacked...")
         if self.hp <= 0:
@@ -16,7 +29,12 @@ class Monster:
             exit()
         else:
             print(f"New hp is {self.hp}")
-            print("argggh..")
-            self.attack(attacker)
+            print("arghhh..")
+            self.attack()
 
 
+m1 = Monster(orange, 190, spider)
+m1.get_hair_color()
+m2 = Monster(blue, 213, slime)
+m2.get_hair_color()
+m2.get_age()
