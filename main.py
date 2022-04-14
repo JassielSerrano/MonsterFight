3f83222e101d96cbdf0206d6f42902a688d49cf7
jassi <jassi@gmail.com>
2022-04-14 14:08:29 -0700

    monster fix

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import random
+import time
 
 
 class Monster:
-    def __init__(self, hp = 30, hair_color, age, type):
+    def __init__(self, hair_color, age, type, hp):
         self.hp = hp
         self.hair_color = hair_color
         self.age = age
@@ -17,24 +18,30 @@ class Monster:
     def get_type(self):
         return self.type
 
+    def get_hp(self):
+        return self.hp
+
     def attack(self, monster):
-        monster.get_attacked(1, 5, self)
+        damage = random.randint(1, 5)
+        monster.get_attacked(damage, self)
 
     def get_attacked(self, damage, attacker):
-        damage = random.randint
         self.hp = self.hp - damage
-        print("Monster got attacked...")
+        print(f"{ attacker.get_hair_color()} hair color monster got attacked...")
+
+        time.sleep(2)
         if self.hp <= 0:
             print("This monster has died")
             exit()
         else:
             print(f"New hp is {self.hp}")
             print("arghhh..")
-            self.attack()
+            self.attack(attacker)
 
-
-m1 = Monster(orange, 190, spider)
-m1.get_hair_color()
-m2 = Monster(blue, 213, slime)
-m2.get_hair_color()
-m2.get_age()
+m1 = Monster("Orange", 190, "Spider", 30)
+print(f"This {m1.get_type()} hair color is {m1.get_hair_color()} and is {m1.get_age()} years old.")
+print(f"Hp of this monster is {m1.get_hp}")
+m2 = Monster("Blue", 213, "Slime", 30)
+print(f"This {m2.get_type()} hair color is {m2.get_hair_color()} and is {m2.get_age()} years old.")
+print(f"Hp of this monster is {m2.get_hp}")
+m1.attack(m2)
