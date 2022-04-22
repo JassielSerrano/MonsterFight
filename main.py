8e458b1a9b579f512f90b56d0a36239bb778cc38
jassi <jassi@gmail.com>
2022-04-22 13:46:01 -0700

    monster final

--- main.py
+++ main.py
@@ -25,18 +25,27 @@ class Monster:
         damage = random.randint(1, 5)
         monster.get_attacked(damage, self)
 
+
     def get_attacked(self, damage, attacker):
         self.hp = self.hp - damage
         print(f"{ attacker.get_hair_color()} hair color monster got attacked...")
+        print(f"This monster has taken {damage} damage")
+        if damage == 1:
+            damage = damage + 1
+            print("Attacker's attack's damage is increased by 1")
+
+        if damage == 5:
+            damage = damage - 1
+            print("This monster's defense has been increased by 1")
 
         time.sleep(2)
         if self.hp <= 0:
-            print("This monster has died")
-            exit()
+            print("This monster has died...")
         else:
             print(f"New hp is {self.hp}")
             print("arghhh..")
             self.attack(attacker)
+
 
 m1 = Monster("Orange", 190, "Spider", 30)
 print(f"This {m1.get_type()} hair color is {m1.get_hair_color()} and is {m1.get_age()} years old.")
@@ -45,3 +54,10 @@ m2 = Monster("Blue", 213, "Slime", 30)
 print(f"This {m2.get_type()} hair color is {m2.get_hair_color()} and is {m2.get_age()} years old.")
 print(f"Hp of this monster is {m2.get_hp}")
 m1.attack(m2)
+
+choice = input("Do you want to play the game again? (y/n)")
+if choice.lower() == 'n':
+    quit()
+if choice.lower() != 'y':
+    print("Not(y/n)")
+    quit()
